e1925545c0f38d49257e5971de66647891dfc7c4
Slry1 <129071869+Slry1@users.noreply.github.com>
2024-01-24 03:30:32 +0500

    Добавлена навигация, стили и шаблонизатор

--- fullstack_programmer/fullstack_programmer/urls.py
+++ fullstack_programmer/fullstack_programmer/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main_app.views import index_page
+from main_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page)
-]
+    path('', views.index_page, name='home'),
+    path('skills', views.skill_page, name='skills')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
